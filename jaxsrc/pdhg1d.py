bc1b10df1253ea81a1238093e839cac3beb374bf
Liu Yang <liuyang.mage@gmail.com>
2023-03-11 11:12:56 -0800

    move time testing to a new file

--- jaxsrc/pdhg1d.py
+++ jaxsrc/pdhg1d.py
@@ -237,7 +237,6 @@ def pdhg_onedim_periodic_iter(f_in_H, c_in_H, tau, sigma, m_prev, rho_prev, mu_p
   return rho_next, phi_next, m_next, mu_next, jnp.array([err1, err2,err3])
 
 
-@utils.timeit
 def pdhg_onedim_periodic_rho_m_EO_L1_xdep(f_in_H, c_in_H, phi0, rho0, m0, mu0, stepsz_param, 
                                           g, dx, dt, c_on_rho, if_precondition, 
                                           N_maxiter = 1000000, print_freq = 1000, eps = 1e-6):
@@ -293,7 +292,7 @@ def pdhg_onedim_periodic_rho_m_EO_L1_xdep(f_in_H, c_in_H, phi0, rho0, m0, mu0, s
     error_all.append(error)
     if error[0] < eps and error[1] < eps:
       break
-    if i % print_freq == 0:
+    if print_freq > 0 and i % print_freq == 0:
       print('iteration {}, primal error with prev step {}, dual error with prev step {}, eqt error {}'.format(i, error[0],  error[1],  error[2]), flush = True)
    
     rho_prev = rho_next
@@ -305,38 +304,40 @@ def pdhg_onedim_periodic_rho_m_EO_L1_xdep(f_in_H, c_in_H, phi0, rho0, m0, mu0, s
 
 
 if __name__ == "__main__":
+  nt = 200
+  nx = 101
+  if_precondition = True
+  N_maxiter = 2001
+  eps = 1e-6
+  T = 1
+  x_period = 2
+  stepsz_param = 0.9
+  c_on_rho = 10.0
+  alpha = 2 * jnp.pi / x_period
+  J = lambda x: jnp.sin(alpha * x)
+  f_in_H_fn = lambda x: 0*x
+  c_in_H_fn = lambda x: 1 + 3* jnp.exp(-4 * (x-1) * (x-1))
 
-  for nt, nx in [(200, 101), (200, 201), (400,201), (400, 401), (800,401), (800,801), (1600,801), (1600,1601)]:
-    if_precondition = True
-    N_maxiter = 2001
-    eps = 1e-6
-    T = 1
-    x_period = 2
-    stepsz_param = 0.9
-    c_on_rho = 10.0
-    alpha = 2 * jnp.pi / x_period
-    J = lambda x: jnp.sin(alpha * x)
-    f_in_H_fn = lambda x: 0*x
-    c_in_H_fn = lambda x: 1 + 3* jnp.exp(-4 * (x-1) * (x-1))
-
-    dx = x_period / (nx)
-    dt = T / (nt-1)
-    x_arr = jnp.linspace(0.0, x_period - dx, num = nx)[None,:]  # [1, nx]
-    g = J(x_arr)  # [1, nx]
-    f_in_H = f_in_H_fn(x_arr)  # [1, nx]
-    c_in_H = c_in_H_fn(x_arr)  # [1, nx]
-      
-    phi0 = einshape("ij->(ki)j", g, k=nt)  # repeat each row of g to nt times, [nt, nx]
+  dx = x_period / (nx)
+  dt = T / (nt-1)
+  x_arr = jnp.linspace(0.0, x_period - dx, num = nx)[None,:]  # [1, nx]
+  g = J(x_arr)  # [1, nx]
+  f_in_H = f_in_H_fn(x_arr)  # [1, nx]
+  c_in_H = c_in_H_fn(x_arr)  # [1, nx]
     
-    rho0 = jnp.zeros([nt-1, nx])
-    m0 = jnp.zeros([nt-1, nx])
-    mu0 = jnp.zeros([1, nx])
+  phi0 = einshape("ij->(ki)j", g, k=nt)  # repeat each row of g to nt times, [nt, nx]
+  
+  rho0 = jnp.zeros([nt-1, nx])
+  m0 = jnp.zeros([nt-1, nx])
+  mu0 = jnp.zeros([1, nx])
 
-    print("nt = {}, nx = {}".format(nt, nx))
-    print("warm up run, 1000 steps:", flush = True) # for each (nt, nx), the first few iterations would be slower due to jit compiling
-    phi_output, error_all = pdhg_onedim_periodic_rho_m_EO_L1_xdep(f_in_H, c_in_H, phi0, rho0, m0, mu0, stepsz_param, 
-                                            g, dx, dt, c_on_rho, if_precondition, N_maxiter = 1000, print_freq = 10000, eps = eps)
-    print("real run, {} steps:".format(N_maxiter), flush = True)
-    phi_output, error_all = pdhg_onedim_periodic_rho_m_EO_L1_xdep(f_in_H, c_in_H, phi0, rho0, m0, mu0, stepsz_param, 
-                                            g, dx, dt, c_on_rho, if_precondition, N_maxiter = N_maxiter, print_freq = N_maxiter//5, eps = eps)
+  #utils.timeit(pdhg_onedim_periodic_rho_m_EO_L1_xdep)(...) to get time
+  phi_output, error_all = pdhg_onedim_periodic_rho_m_EO_L1_xdep(f_in_H, c_in_H, phi0, rho0, m0, mu0, stepsz_param, 
+                                          g, dx, dt, c_on_rho, if_precondition, N_maxiter = N_maxiter, print_freq = N_maxiter//5, eps = eps)
 
+  # iteration 0, primal error with prev step 0.0, dual error with prev step 2.1361549842616188, eqt error 4.790281222746855
+  # iteration 400, primal error with prev step 0.20220652147305648, dual error with prev step 1.6410945674460602, eqt error 2.502589916793291
+  # iteration 800, primal error with prev step 0.06831688412331721, dual error with prev step 1.5780906564243886, eqt error 2.0334908931956353
+  # iteration 1200, primal error with prev step 0.11312863344242437, dual error with prev step 1.3012967388228125, eqt error 3.0772971015694264
+  # iteration 1600, primal error with prev step 0.13333274128396508, dual error with prev step 1.1886147450803772, eqt error 2.796169857846648
+  # iteration 2000, primal error with prev step 0.0751826853549036, dual error with prev step 1.359449566705966, eqt error 2.0459435047193817
