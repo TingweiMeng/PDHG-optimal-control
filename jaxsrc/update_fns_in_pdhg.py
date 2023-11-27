f98fa556c783acddfbfca835c444619f4ca1fc0e
Tingwei Meng <mengtingwei.math@gmail.com>
2023-11-26 21:19:07 -0800

    [src] C = 0, pow = 1/2

--- jaxsrc/update_fns_in_pdhg.py
+++ jaxsrc/update_fns_in_pdhg.py
@@ -129,7 +129,7 @@ def update_alp_2d(alp_prev, phi, rho, sigma, dspatial, fns_dict, x_arr, t_arr, e
 def update_primal_1d(phi_prev, rho_prev, c_on_rho, alp_prev, tau, dt, dspatial, fns_dict, fv, epsl, x_arr, t_arr):
   delta_phi = compute_cont_residual_1d(rho_prev, alp_prev, dt, dspatial, fns_dict, c_on_rho, epsl, x_arr, t_arr)
   C = 0.0
-  pow = 1
+  pow = 0.5
   phi_next = phi_prev + tau * H1_precond_1d(delta_phi, fv, dt, C = C, pow = pow)
   return phi_next
 
