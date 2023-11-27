1b55e314ee397be7ece662c44adef175e2584d16
Tingwei Meng <mengtingwei.math@gmail.com>
2023-11-26 21:15:55 -0800

    [src] pow 1/2

--- jaxsrc/update_fns_in_pdhg.py
+++ jaxsrc/update_fns_in_pdhg.py
@@ -129,7 +129,8 @@ def update_alp_2d(alp_prev, phi, rho, sigma, dspatial, fns_dict, x_arr, t_arr, e
 def update_primal_1d(phi_prev, rho_prev, c_on_rho, alp_prev, tau, dt, dspatial, fns_dict, fv, epsl, x_arr, t_arr):
   delta_phi = compute_cont_residual_1d(rho_prev, alp_prev, dt, dspatial, fns_dict, c_on_rho, epsl, x_arr, t_arr)
   C = 1.0
-  phi_next = phi_prev + tau * H1_precond_1d(delta_phi, fv, dt, C = C)
+  pow = 0.5
+  phi_next = phi_prev + tau * H1_precond_1d(delta_phi, fv, dt, C = C, pow = pow)
   return phi_next
 
 @partial(jax.jit, static_argnames=("fns_dict",))
